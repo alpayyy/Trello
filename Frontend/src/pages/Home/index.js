d9fbe8798c0609817205439a7d60b6c74d8ff881
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-07-25 13:42:13 +0300

    Added authentication check to Home page

--- Frontend/src/pages/Home/index.js
+++ Frontend/src/pages/Home/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import KanbanBoard from "../../components/Kanban/KanbanBoard";
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  // Redux store'dan verileri almak için useSelector kullanıyoruz
   const lists = useSelector((state) => state.kanban.lists);
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
 
   return (
     <Container>
-     
-      <KanbanBoard lists={lists} />
+      {isAuthenticated ? (
+        <KanbanBoard lists={lists} />
+      ) : (
+        <Box sx={{
+          marginTop: 15,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+          <Typography variant="h2" align="center" gutterBottom>
+            Lütfen giriş yapın
+          </Typography>
+          <Link to={"/login"} >
+            <Button sx={{marginTop: 5,}} variant="contained" color="primary" >
+              Giriş Yap
+            </Button>
+          </Link >
+        </Box>
+      )}
     </Container>
   );
 }
